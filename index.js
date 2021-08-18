c3a3ca64d2acb1ff06d860d2167a139f70d03f44
MaxLyFe <85650989+maxlyfe@users.noreply.github.com>
2021-08-17 22:22:17 -0300

    Random de mng no home

--- index.js
+++ index.js
@@ -14,9 +14,22 @@ const games = [ //Crio uma constante 'lista' com o nome games e 4 itens dentro, 
     'Ragnarok',
     'Perfect World'
 ];
+const mngInicio = [
+    'Bem vindo',
+    'Bem vindo a nosso site de Jogos',
+    'Nosso servidor de Jogos!',
+    'Quem é você? Sai do meu servidor.'
+];
+function randomMinMax(min, max) {
+    return Math.floor(Math.random() * (max - min )) + min;
+};
+function frase(num){
+    return mngInicio[num];
+};
+console.log(frase(randomMinMax(0,3)));
 
 app.get('/', (req, res) =>{ //criamos a rota '/' inicial e vamos enviar uma mensagem inicial.
-    res.send('Bem vindo a nosso site de Jogos');
+    res.send(`<h1>${frase(randomMinMax(0,3))}</h1>`);
 });
 
 app.get('/games', (req, res) =>{ //criamos a rota /games e dentro dela a lista de jogos
@@ -27,6 +40,10 @@ app.get('/games/:id', (req,res) => { //crio a rota games + o id que será escoli
     const id = req.params.id -1; // coloco um -1 para que fique mas interativo 
     const game = games[id];
     res.send(game);
+});
+
+games.forEach(function (item, indice){
+    console.log(indice, item);
 });
 
 app.listen(port, () => {
