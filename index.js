c869d2378d3647239e3ac2315402fc31a7dfb86a
MaxLyFe <85650989+maxlyfe@users.noreply.github.com>
2021-08-17 21:24:05 -0300

    Projeto_02_Modulo_03

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express'); //importa express
+const app = express(); // informa que neste app vamos utilizar o express
+
+const port = 3000; //crio uma constante com nome port e ela recebe o valor 3000
+
+const games = [ //Crio uma constante 'lista' com o nome games e 4 itens dentro, a mesma começa com o indice 0
+    'Pokémon Go',
+    'Mobile Legends Bang Bang',
+    'Ragnarok',
+    'Perfect World'
+];
+
+app.get('/', (req, res) =>{ //criamos a rota '/' inicial e vamos enviar uma mensagem inicial.
+    res.send('Bem vindo a nosso site de Jogos');
+});
+
+app.get('/games', (req, res) =>{ //criamos a rota /games e dentro dela a lista de jogos
+    res.send(games);
+});
+
+app.get('/games/:id', (req,res) => { //crio a rota games + o id que será escolido pelo usuario
+    const id = req.params.id -1; // coloco um -1 para que fique mas interativo 
+    const game = games[id];
+    res.send(game);
+});
+
+app.listen(port, () => {
+    console.info(`Nosso app esta rodando em: http://localhost:${port}/`); //crio uma mensagem no console para confirmar que meu app esta rodando.
+});
